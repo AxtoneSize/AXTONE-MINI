c4429083f64dbc174de4a6b99be3e27603dd36c5
chentianzhu <chentianzhu@tastien.com>
2023-11-28 17:00:50 +0800

    feat: 集成redis&继承hutool

--- axtone-web/src/main/java/com/size/admin/AxtoneWebApplication.java
+++ axtone-web/src/main/java/com/size/admin/AxtoneWebApplication.java
@@ -1,18 +1,20 @@
 package com.size.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 /**
  * @Description:
  * @Author: CTZ
  * @Date: 2023/11/17
  */
+@Slf4j
 @SpringBootApplication(scanBasePackages = {"com.size.admin"})
 public class AxtoneWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AxtoneWebApplication.class);
+        log.info(AxtoneWebApplication.class.getSimpleName() + " Start Successful");
     }
 }
